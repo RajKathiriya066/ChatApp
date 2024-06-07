decdf95055031af72ac7a294668209d040910156
RK066 <rajkathiriya66@gmail.com>
2024-06-07 11:41:28 +0530

    initial commit

---
+++ app/src/main/java/com/example/chatz/listeners/ConversionListener.java
@@ -0,0 +1,8 @@
+package com.example.chatz.listeners;
+
+import com.example.chatz.models.User;
+
+public interface ConversionListener {
+    void onConversionClicked(User user);
+}
+
---
+++ app/src/main/java/com/example/chatz/listeners/UserListener.java
@@ -0,0 +1,7 @@
+package com.example.chatz.listeners;
+
+import com.example.chatz.models.User;
+
+public interface UserListener {
+    void onUserClicked(User user);
+}
